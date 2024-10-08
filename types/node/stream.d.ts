55c2768d5ccfa5db8b4b3861da0d44fe3c0f5649
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2024-10-08 22:35:35 +0100

    Fix base Stream prototype
    Move .compose() to Readable

--- types/node/stream.d.ts
+++ types/node/stream.d.ts
@@ -56,22 +56,80 @@ declare module "stream" {
 
     type ComposeFnParam = (source: any) => void;
 
+    /**
+     * @since v0.3.0
+     */
     class Stream extends EventEmitter {
+        /**
+         * The `readable.pipe()` method attaches a `Writable` stream to the `readable`,
+         * causing it to switch automatically into flowing mode and push all of its data
+         * to the attached `Writable`. The flow of data will be automatically managed
+         * so that the destination `Writable` stream is not overwhelmed by a faster
+         * `Readable` stream.
+         *
+         * The following example pipes all of the data from the `readable` into a file
+         * named `file.txt`:
+         *
+         * ```js
+         * import * as fs from 'node:fs';
+         * const readable = getReadableStreamSomehow();
+         * const writable = fs.createWriteStream('file.txt');
+         * // All the data from readable goes into 'file.txt'.
+         * readable.pipe(writable);
+         * ```
+         *
+         * It is possible to attach multiple `Writable` streams to a single `Readable`
+         * stream.
+         *
+         * The `readable.pipe()` method returns a reference to the _destination_ stream
+         * making it possible to set up chains of piped streams:
+         *
+         * ```js
+         * import * as fs from 'node:fs';
+         * import * as zlib from 'node:zlib';
+         * const r = fs.createReadStream('file.txt');
+         * const z = zlib.createGzip();
+         * const w = fs.createWriteStream('file.txt.gz');
+         * r.pipe(z).pipe(w);
+         * ```
+         *
+         * By default, `stream.end()` is called on the destination `Writable`
+         * stream when the source `Readable` stream emits `'end'`, so that the
+         * destination is no longer writable. To disable this default behavior, the `end`
+         * option can be passed as `false`, causing the destination stream to remain open:
+         *
+         * ```js
+         * reader.pipe(writer, { end: false });
+         * reader.on('end', () => {
+         *   writer.end('Goodbye\n');
+         * });
+         * ```
+         *
+         * One important caveat is that if the `Readable` stream emits an error during
+         * processing, the `Writable` destination _is not closed_ automatically. If an
+         * error occurs, it will be necessary to _manually_ close each stream in order
+         * to prevent memory leaks.
+         *
+         * The `process.stderr` and `process.stdout` `Writable` streams are never
+         * closed until the Node.js process exits, regardless of the specified options.
+         * @since v0.3.0
+         */
         pipe<T extends NodeJS.WritableStream>(
             destination: T,
-            options?: {
-                end?: boolean | undefined;
-            },
-        ): T;
-        compose<T extends NodeJS.ReadableStream>(
-            stream: T | ComposeFnParam | Iterable<T> | AsyncIterable<T>,
-            options?: { signal: AbortSignal },
+            options?: Stream.PipeOptions,
         ): T;
     }
     namespace Stream {
         export { promises, Stream };
     }
     namespace Stream {
+        interface PipeOptions {
+            /**
+             * End the writer when the reader ends.
+             * @default true
+             */
+            end?: boolean | undefined;
+        }
         interface StreamOptions<T extends Stream> extends Abortable {
             emitClose?: boolean | undefined;
             highWaterMark?: number | undefined;
@@ -474,6 +532,10 @@ declare module "stream" {
              */
             wrap(stream: NodeJS.ReadableStream): this;
             push(chunk: any, encoding?: BufferEncoding): boolean;
+            compose<T extends NodeJS.ReadableStream>(
+                stream: T | ComposeFnParam | Iterable<T> | AsyncIterable<T>,
+                options?: { signal: AbortSignal },
+            ): T;
             /**
              * The iterator created by this method gives users the option to cancel the destruction
              * of the stream if the `for await...of` loop is exited by `return`, `break`, or `throw`,
